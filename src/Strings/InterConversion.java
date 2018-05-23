960f6cd311aad834adbb80a85c5b648e1182dd78
Krishna <kd82@zips.uakron.edu>
2018-05-23 00:33:32 -0400

    Added String problems

---
+++ src/Strings/InterConversion.java
@@ -0,0 +1,43 @@
+package Strings;
+
+public class InterConversion {
+
+	public static void main(String[] args) {
+	 System.out.println(intToString(-101));
+	 System.out.println(stringToInteger("-101"));
+	}
+
+	private static int stringToInteger(String str) {
+		boolean isNegative=str.charAt(0)=='-';
+		int result=0;
+		int i=isNegative?1:0;
+		while(i<str.length())
+		{
+			int digit=str.charAt(i)-'0';
+			result=result*10+digit;
+			i++;
+		}
+		return isNegative?-result:result;
+	}
+
+	private static String intToString(int n) {
+	  boolean isNegative=false;
+	  if(n<0)
+	  {
+		  n=-n;
+		  isNegative=true;
+	  }
+	  StringBuilder sb=new StringBuilder();
+	  do{
+		sb.append((char)('0'+(n%10)));
+		n/=10;
+	  }while(n!=0);
+	 
+	  if(isNegative)
+		 {
+		  sb.append("-");
+		 }
+	  sb.reverse();
+	  return sb.toString();
+	}
+}
---
+++ src/Strings/Palindrome.java
@@ -0,0 +1,55 @@
+package Strings;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+	  String[] input={"abA","casper","magam","toledo","kingsgnik","loL"};
+       for(String in:input)
+       {
+    	   System.out.println("Iterative ");
+    	   System.out.print(isPalindromic(in)+" ");
+    	   System.out.println("Recursive ");
+    	   System.out.print(isPalindromicRecursive(in)+" ");
+    	   System.out.println("Case Sensitive ");
+    	   System.out.print(isPalindrome(in)+" ");
+       }
+	}
+
+	private static boolean isPalindromic(String str) {
+		for(int i=0,j=str.length()-1;i<j;++i,--j){
+			if(str.charAt(i)!=str.charAt(j))
+				return false;
+		}
+		return true;
+	}
+   // Recursive Approach
+   public static boolean isPalindromicRecursive(String str)
+   {
+	   if(str.length()<1)
+		   return true;
+	   if(str.charAt(0)!=str.charAt(str.length()-1))
+		   return false;
+	   return true;
+   }
+   //Case insensitive
+   public static boolean isPalindrome(String s)
+   {
+	   int i=0,j=s.length()-1;
+	   while(i<j){
+		   if(!Character.isLetterOrDigit(s.charAt(i)) && i<j)
+		   {
+			   i++;
+		   }
+		   if(!Character.isLetterOrDigit(s.charAt(j)) && i<j)
+		   {
+			   j--;
+		   }
+		   if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j)))
+			   return false;
+		   ++i;
+		   --j;
+	   }
+	   return true;
+   }
+
+}
