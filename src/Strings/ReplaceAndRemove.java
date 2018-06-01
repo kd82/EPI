9e85e90c533289824583a2e47c8246ca770944a9
Krishna <kd82@zips.uakron.edu>
2018-06-01 00:49:16 -0400

    Added Some Interesting String Problems

---
+++ src/Strings/ReplaceAndRemove.java
@@ -0,0 +1,40 @@
+package Strings;
+
+public class ReplaceAndRemove {
+
+	public static void main(String[] args) {
+		char[] input={'a','c','d','b','b','c','a'};
+		replaceAndRemove(7, input);
+	}
+	public static int replaceAndRemove(int size, char[] s) {
+	    // TODO - you fill in here.
+		//Remove b first and at the same time get the count of a 
+		int aCount=0,writeIdx=0;
+		for(int i=0;i<size;++i){
+			if(s[i]=='a')
+				++aCount;
+			if(s[i]!='b'){
+				s[writeIdx++]=s[i];
+			}
+		}
+		
+		int curIdx=writeIdx-1;
+		writeIdx=writeIdx+aCount-1;
+		int finalLen=writeIdx+1;
+		while(curIdx>=0){
+			if(s[curIdx]=='a'){
+				s[writeIdx--]='d';
+				s[writeIdx--]='d';
+			}
+			else{
+				s[writeIdx--]=s[curIdx];
+			}
+			--curIdx;
+		}
+		for(int i=0;i<finalLen;++i){
+				System.out.print(s[i]+" ");
+		}
+	    return finalLen;
+	  }
+
+}
---
+++ src/Strings/TelexEncoding.java
@@ -0,0 +1,37 @@
+package Strings;
+
+public class TelexEncoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] input={'.','b','c', 'd','e', '.',' ',' ',' ',' ', ' ', ' '};
+		telexEncoding(6, input);
+	}
+
+	private static void telexEncoding(int size, char[] input) {
+	  final String[] spelledChars={"TOD","AMMOC","KRAM NOITSEUQ","KRAM NOITAMALCXE"};
+      int count=0;
+	  for(int i=0;i<size;++i){
+		  if(input[i]=='.'){
+			  count+=spelledChars[0].length();
+		  }
+      }
+	  int finalSize=size+count-1;
+	  int writeIdx=finalSize,j=size;
+	  while(j>=0){
+		  if(input[j]=='.'){
+			  input[--writeIdx]='T';
+			  input[--writeIdx]='O';
+			  input[--writeIdx]='D';
+		  }
+		  else{
+			  input[--writeIdx]=input[j];
+		  }
+		  --j;
+	  }
+	  for(int i=0;i<finalSize;i++){
+		  System.out.println(input[i]);
+	  }
+	}
+
+}
