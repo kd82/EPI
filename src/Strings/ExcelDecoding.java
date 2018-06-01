7cca377dfc244557f0942e585c53609533031c43
kd82 <kd82@zips.uakron.edu>
2018-06-01 00:49:16 -0400

    Added Some Interesting String Problems

---
+++ src/Strings/ExcelDecoding.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class ExcelDecoding {
+
+	public static void main(String[] args) {
+	  System.out.println(ssDecodeColID("AA"));
+	}
+	 public static int ssDecodeColID(final String str) {
+		    int ret=0;
+		    for(int i=0;i<str.length();++i){
+		    	char c=str.charAt(i);
+		    	ret=ret*26+c-'A'+1;
+		    }
+		    return ret;
+	 }
+}
