fcfd11bc82e310e3947b73f4f4fcf62b0be7ba9c
kd82 <kd82@zips.uakron.edu>
2018-06-07 01:38:53 -0400

    Linked Lists and Merge Two sorted Linked Lists

--- src/Strings/RabinKarp.java
+++ src/Strings/RabinKarp.java
@@ -4,13 +4,32 @@ public class RabinKarp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println(rabinKarp("GACGCCA","CGC"));
+      System.out.println(rabinKarp("GACGCCA","GAC"));
 	}
 	private static int rabinKarp(String t,String p){
 	   if(p.length()>t.length())
 		return -1;
-	   
+	  final int BASE=256;
+	  int powerS=1;
+	  int tHash=0,pHash=0;
+	  for(int i=0;i<p.length();++i){
+		  powerS=i>0?powerS*BASE:1;
+		  tHash=tHash*BASE+t.charAt(i);
+		  pHash=pHash*BASE+p.charAt(i);
+	  }
+	  //Collision Check to see if the strings are really equal or not
+	  for(int i=p.length();i<t.length();i++){
+		  if(tHash==pHash && t.substring(i-p.length(),i).equals(p)){
+			return i-p.length();  
+		  }
+		  tHash-=t.charAt(i-p.length())*powerS;
+		  tHash=tHash*BASE+t.charAt(i);
+	  }
+	  //Check to see if the strings are equal 
+	  if(tHash==pHash && t.substring(t.length()-p.length()).equals(p)){
+			return t.length()-p.length();  
+		  }
 	   return -1;
 	}
-
+    
 }
---
+++ src/linkedLists/ListNode.java
@@ -0,0 +1,35 @@
+package linkedLists;
+
+public class ListNode<T> {
+	public T data;
+	public ListNode<T> next;
+	public ListNode<T> prev;
+	
+	// For Single LinkedList(SLL)
+	public ListNode(T data, ListNode<T> next) {
+	   this.data=data;
+	   this.next=next;
+	}
+	// For Double LinkedList(DLL)
+	public ListNode(T data, ListNode<T> next,ListNode<T> prev) {
+		   this.data=data;
+		   this.next=next;
+		   this.prev=prev;
+	}
+	// Search for the given Key
+	public ListNode<T> Search(ListNode<T> node,T key){
+		while(node.next!=null && node.data!=key){
+			node=node.next;
+		}
+		return node;
+	}
+	//Inserts After a given Node
+	public void InsertAfter(ListNode<T> node,ListNode<T> newNode){
+		newNode.next=node.next;
+		node.next=newNode;
+	}
+	//deletes the given Node
+	public void Delete(ListNode<T> anode){
+		anode.next=anode.next.next;
+	}
+}
