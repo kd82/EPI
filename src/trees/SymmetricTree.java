ebe2071f523326e7e60abd122bf05c82efbce458
kd82 <kd82@zips.uakron.edu>
2019-01-23 23:26:35 -0500

    Removed unused code and added EPI Judge

---
+++ src/trees/SymmetricTree.java
@@ -0,0 +1,14 @@
+package trees;
+
+public class SymmetricTree {
+
+	public SymmetricTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
