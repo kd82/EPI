ed67ce6aef9d1d379f5d40cedf43b699df46eb5f
Krishna <kd82@zips.uakron.edu>
2019-01-30 00:20:08 -0500

    Added package for Binary Search Trees

--- src/trees/BinaryTreeNode.java
+++ src/trees/BinaryTreeNode.java
@@ -1,12 +1,14 @@
 package trees;
 
+import binarySearchTrees.BSTNode;
+
 public class BinaryTreeNode<T> {
 	public T data;
     public BinaryTreeNode<T> left,right;
-    
-    public BinaryTreeNode(T item) 
-    { 
-        this.data = item; 
-        this.left = this.right = null; 
+
+    public BinaryTreeNode(T item)
+    {
+        this.data = item;
+        this.left = this.right = null;
     }
 }
